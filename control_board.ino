9b7b490aa33d5f24c8a541692bf84acdaff2be44
jam1092 <156978971+jam1092@users.noreply.github.com>
2025-02-14 13:52:36 -0500

    Add files via upload

---
+++ control_board.ino
@@ -0,0 +1,97 @@
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin1 = 2;  // the number of the pushbutton pin
+const int buttonPin2 = 3;  // the number of the pushbutton pin
+const int buttonPin3 = 4;  // the number of the pushbutton pin
+const int buttonPin4 = 5;  // the number of the pushbutton pin
+
+const int ledPin1 = 13;    // the number of the LED pin
+const int ledPin2 = 12;    // the number of the LED pin
+const int ledPin3 = 11;    // the number of the LED pin
+const int ledPin4 = 10;    // the number of the LED pin
+
+// variables will change:
+int buttonState1 = 0;  // variable for reading the pushbutton status
+int buttonState2 = 0;  // variable for reading the pushbutton status
+int buttonState3 = 0;  // variable for reading the pushbutton status
+int buttonState4 = 0;  // variable for reading the pushbutton status
+
+int buttonRead1 = 0;  // variable for reading the pushbutton status
+int buttonRead2 = 0;  // variable for reading the pushbutton status
+int buttonRead3 = 0;  // variable for reading the pushbutton status
+int buttonRead4 = 0;  // variable for reading the pushbutton status
+
+int oldRead1 = 0;  // variable for reading the pushbutton status
+int oldRead2 = 0;  // variable for reading the pushbutton status
+int oldRead3 = 0;  // variable for reading the pushbutton status
+int oldRead4 = 0;  // variable for reading the pushbutton status
+
+
+void setup() {
+  // initialize the LED pin as an output:
+  Serial.begin(9600);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+  pinMode(buttonPin3, INPUT);
+  pinMode(buttonPin4, INPUT);
+
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonRead1 = digitalRead(buttonPin1);
+  buttonRead2 = digitalRead(buttonPin2);
+  buttonRead3 = digitalRead(buttonPin3);
+  buttonRead4 = digitalRead(buttonPin4);
+
+  if((buttonRead1 == HIGH) && (oldRead1 == 0)){
+    if(buttonState1 == 1){
+      buttonState1 = 0;
+    }
+    else {
+      buttonState1 = 1;
+      }
+  }
+
+  if((buttonRead2 == HIGH) && (oldRead2 == 0)){
+    if(buttonState2 == 1){
+      buttonState2 = 0;
+    }
+    else {
+      buttonState2 = 1;
+      }
+  }
+
+  if((buttonRead3 == HIGH) && (oldRead3 == 0)){
+    if(buttonState3 == 1){
+      buttonState3 = 0;
+    }
+    else {
+      buttonState3 = 1;
+      }
+  }
+
+  if((buttonRead4 == HIGH) && (oldRead4 == 0)){
+    if(buttonState4 == 1){
+      buttonState4 = 0;
+    }
+    else {
+      buttonState4 = 1;
+      }
+  }
+  digitalWrite(ledPin1, buttonState1);
+  digitalWrite(ledPin2, buttonState2);
+  digitalWrite(ledPin3, buttonState3);
+  digitalWrite(ledPin4, buttonState4);
+
+  oldRead1 = buttonRead1;
+  oldRead2 = buttonRead2;
+  oldRead3 = buttonRead3;
+  oldRead4 = buttonRead4;
+
+}
