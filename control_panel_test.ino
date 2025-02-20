64144bee91c3fc3ba1ba07cdca6468f79dd613b0
Tomingus <132331472+Tomingus@users.noreply.github.com>
2025-02-19 19:30:52 -0500

    Update control_panel_test.ino

--- control_panel_test.ino
+++ control_panel_test.ino
@@ -1,28 +1,28 @@
 #define NUM_BUTTONS 4
 
-int buttonPins[NUM_BUTTONS] = {A0, A1, A2, A3};  // Button input pins
-int ledPins[NUM_BUTTONS] = {6, 7, 8, 9};         // LED output pins
-bool ledState[NUM_BUTTONS] = {false, false, false, false}; // Track LED states
-bool buttonPressed[NUM_BUTTONS] = {false, false, false, false}; // Track button press state
+int buttonPins[NUM_BUTTONS] = {A0, A1, A2, A3};
+int ledPins[NUM_BUTTONS] = {6, 7, 8, 9};
+bool ledState[NUM_BUTTONS] = {false, false, false, false};
+bool buttonPressed[NUM_BUTTONS] = {false, false, false, false};
 
 void setup() {
   for (int i = 0; i < NUM_BUTTONS; i++) {
-    pinMode(buttonPins[i], INPUT_PULLUP);  // Enable internal pull-up resistor
+    pinMode(buttonPins[i], INPUT_PULLUP);
     pinMode(ledPins[i], OUTPUT);
-    digitalWrite(ledPins[i], LOW); // Ensure LEDs start OFF
+    digitalWrite(ledPins[i], LOW);
   }
 }
 
 void loop() {
   for (int i = 0; i < NUM_BUTTONS; i++) {
-    if (digitalRead(buttonPins[i]) == LOW) { // Button Pressed
-      if (!buttonPressed[i]) {  // Only trigger if it wasn't already pressed
-        buttonPressed[i] = true;  // Mark button as pressed
-        ledState[i] = !ledState[i]; // Toggle LED state
-        digitalWrite(ledPins[i], ledState[i] ? HIGH : LOW); // Update LED
+    if (digitalRead(buttonPins[i]) == LOW) {
+      if (!buttonPressed[i]) {
+        buttonPressed[i] = true;
+        ledState[i] = !ledState[i];
+        digitalWrite(ledPins[i], ledState[i] ? HIGH : LOW); 
       }
     } else {
-      buttonPressed[i] = false; // Reset button press flag when released
+      buttonPressed[i] = false;
     }
   }
 }
